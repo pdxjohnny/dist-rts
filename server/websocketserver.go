a1ef552452534ee03bd0e924bdeccb77a376adad
John Andersen <johnandersenpdx@gmail.com>
2015-08-06 13:57:45 -0700

    Added javascript client

--- server/websocketserver.go
+++ server/websocketserver.go
@@ -3,33 +3,17 @@ package server
 import (
 	"fmt"
 	"net/http"
-	"text/template"
+
+	"github.com/GeertJohan/go.rice"
+	"github.com/pdxjohnny/easysock"
 
 	"github.com/pdxjohnny/dist-rts/config"
-	"github.com/pdxjohnny/easysock"
 )
-
-func ServeHome(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", 404)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", 405)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	homeTempl, err := template.ParseFiles("../static/home.html")
-	if err != nil {
-		homeTempl, err = template.ParseFiles("static/home.html")
-	}
-	homeTempl.Execute(w, r.Host)
-}
 
 func Run() error {
 	conf := config.Load()
 	go easysock.Hub.Run()
-	http.HandleFunc("/", ServeHome)
+	http.Handle("/", http.FileServer(rice.MustFindBox("../static").HTTPBox()))
 	http.HandleFunc("/ws", easysock.ServeWs)
 	port := fmt.Sprintf(":%s", conf.Port)
 	err := http.ListenAndServe(port, nil)
